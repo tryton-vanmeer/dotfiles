90854afff0b48d0423bf72d6650f3ff5a1f2fd21
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-01-03 20:32:14 -0500

    Updated prompt

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -10,21 +10,22 @@ _prompt_git ()
     git status --porcelain -sb &> /dev/null || return
 
     git_branch="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
-    git status --porcelain | grep "??" &> /dev/null && git_branch+=" " # New Files
-    git status --porcelain | grep "M" &> /dev/null && git_branch+=" "  # Commits
-    [ $(git rev-list origin/$(git rev-parse --abbrev-ref HEAD)..HEAD --count 2> /dev/null) -ne 0 2> /dev/null ] && git_branch+=" "   # Commits ahead of remote
-    [ $(git rev-list HEAD..origin/$(git rev-parse --abbrev-ref HEAD) --count 2> /dev/null) -ne 0 2> /dev/null ] && git_branch+=" "   # Commits behind of remote
+    if [[ $(git status --porcelain) ]]; then
+        git_branch+=" *"
+    fi
+    [ $(git rev-list origin/$(git rev-parse --abbrev-ref HEAD)..HEAD --count 2> /dev/null) -ne 0 2> /dev/null ] && git_branch+=" ↑"   # Commits ahead of remote
+    [ $(git rev-list HEAD..origin/$(git rev-parse --abbrev-ref HEAD) --count 2> /dev/null) -ne 0 2> /dev/null ] && git_branch+=" ↓"   # Commits behind of remote
 
-    echo "\[$(tput setaf 2)\] [ ${git_branch} ]"
+    echo "\[$(tput setaf 2)\] (${git_branch})"
 }
 
 _prompt_python ()
 {
-    [ -z ${VIRTUAL_ENV+x} ] || echo "\[$(tput setaf 3)\]  "
+    [ -z ${VIRTUAL_ENV+x} ] || echo "\[$(tput setaf 3)\]  "
 }
 
 _update_prompt ()
 {
-    PS1="┌\[$(tput setaf 1)\]  \[$(tput setaf 5)\]\w$(_prompt_git )$(_prompt_python)\[$(tput sgr0)\]\n└>$ "
+    PS1="\[$(tput setaf 1)\] \[$(tput setaf 5)\]\w$(_prompt_git )$(_prompt_python)\n\[$(tput setaf 6)\]$ \[$(tput sgr0)\]"
 }
 PROMPT_COMMAND="_update_prompt;$PROMPT_COMMAND"
