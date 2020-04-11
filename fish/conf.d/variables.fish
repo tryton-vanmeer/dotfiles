34a44d74cd0daf9f2483afc70ea3c759804ed64e
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-04-11 09:50:05 -0400

    Set XDG_DATA_HOME.

--- fish/conf.d/variables.fish
+++ fish/conf.d/variables.fish
@@ -4,4 +4,6 @@ set -U fish_emoji_width 2
 set -x BROWSER /usr/bin/firefox
 set -x EDITOR /usr/bin/nano
 
+set -x XDG_DATA_HOME "$HOME"/.local/share/
+
 set -x XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
