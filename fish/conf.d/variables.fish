71eaed2ddb3496e9494eed74e67f8e84197cf8ee
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-04-11 09:54:16 -0400

    Added some comments.

--- fish/conf.d/variables.fish
+++ fish/conf.d/variables.fish
@@ -1,10 +1,13 @@
 set -U fish_user_paths ~/.local/bin $fish_user_paths
 set -U fish_emoji_width 2
 
+# Default Programs
 set -x BROWSER /usr/bin/firefox
 set -x EDITOR /usr/bin/nano
 
+# XDG
 set -x XDG_DATA_HOME "$HOME"/.local/share
 
+# Dotfiles
 set -x XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
 set -x WINEPREFIX "$XDG_DATA_HOME"/wineprefixes/default
