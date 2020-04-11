b3f062bfbbb0bbfb243baecb8ccbe6c30562ecd7
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-04-11 09:52:58 -0400

    Set default WINEPREFIX.

--- fish/conf.d/variables.fish
+++ fish/conf.d/variables.fish
@@ -4,6 +4,7 @@ set -U fish_emoji_width 2
 set -x BROWSER /usr/bin/firefox
 set -x EDITOR /usr/bin/nano
 
-set -x XDG_DATA_HOME "$HOME"/.local/share/
+set -x XDG_DATA_HOME "$HOME"/.local/share
 
 set -x XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
+set -x WINEPREFIX "$XDG_DATA_HOME"/wineprefixes/default
