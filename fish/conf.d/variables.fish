3ba59874fb7311156a5c435303b6932ef42e96fc
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-04-11 10:00:18 -0400

    Added XDG_CACHE_HOME.

--- fish/conf.d/variables.fish
+++ fish/conf.d/variables.fish
@@ -8,6 +8,7 @@ set -x EDITOR /usr/bin/nano
 # XDG
 set -x XDG_DATA_HOME "$HOME/.local/share"
 set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_CACHE_HOME "$HOME/.cache"
 
 # Dotfiles
 set -x XAUTHORITY "$XDG_RUNTIME_DIR/Xauthority"
