6087cdc2de391a127fbe53ba296c241a74a30faa
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-06-27 22:13:46 -0400

    Added fish function for getting the appid of installed steam games.

---
+++ roles/fish/files/functions/steam_get_appid.fish
@@ -0,0 +1,8 @@
+function steam_get_appid
+    # Grep appmanifest files in all Steam libraries for games with $arg in name
+
+    grep -Ei "name.*$argv[1]" $steam_dirs \
+    /opt/Steam/steamapps/appmanifest_*.acf \
+    ~/.steam/steam/steamapps/appmanifest_*.acf \
+    /mnt/HDD/Steam/steamapps/appmanifest_*.acf
+end
