1753deffb6c6cf1bcaaf5e282966eaf6648d8bf9
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-11-05 11:22:04 -0500

    Fix RUSTC_WRAPPER var.

--- roles/fish/files/conf.d/variables.fish
+++ roles/fish/files/conf.d/variables.fish
@@ -8,7 +8,7 @@ set -x EDITOR /usr/bin/micro
 # App Settings
 set -x MICRO_TRUECOLOR 1
 set -x DOTNET_CLI_TELEMETRY_OPTOUT "1"
-set -x RUSTC_WRAPPER=sccache
+set -x RUSTC_WRAPPER "sccache"
 
 # XDG
 set -x XDG_DATA_HOME "$HOME/.local/share"
