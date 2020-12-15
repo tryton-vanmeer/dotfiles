465447a740882b789f13bea87c501d57391438d0
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-12-15 13:31:14 -0500

    Added completion for tag argument.

--- roles/fish/files/completions/podrun.fish
+++ roles/fish/files/completions/podrun.fish
@@ -1,4 +1,17 @@
-set -l distros fedora ubuntu centos archlinux
+# Images
+set -l images fedora ubuntu centos archlinux
+
+# Tags
+set -l fedora 33 rawhide
+set -l ubuntu 20.04 20.10
+set -l centos 7 8
+set -l archlinux base base-devel
 
 # First argument: DISTRO
-complete -f -c podrun -n "not __fish_seen_subcommand_from $distros" -a "$distros"
+complete -f -c podrun -n "not __fish_seen_subcommand_from $images" -a "$images"
+
+# Optional second argument for distro version.
+complete -f -c podrun -n "__fish_seen_subcommand_from fedora" -a "$fedora"
+complete -f -c podrun -n "__fish_seen_subcommand_from ubuntu" -a "$ubuntu"
+complete -f -c podrun -n "__fish_seen_subcommand_from centos " -a "$centos"
+complete -f -c podrun -n "__fish_seen_subcommand_from archlinux" -a "$archlinux"
