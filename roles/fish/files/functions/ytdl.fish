5560999c395cc641f205167f5cbb2221546561ad
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-12-20 06:39:00 -0500

    CHange format of filenames. Date first for sorting.

--- roles/fish/files/functions/ytdl.fish
+++ roles/fish/files/functions/ytdl.fish
@@ -1,5 +1,5 @@
 function ytdl -w "youtube-dl"
     youtube-dl -f '(bestvideo+bestaudio/best)[protocol^=http]' \
-    -o '~/Videos/YouTube/%(uploader)s - %(title)s [%(upload_date)s].%(ext)s' \
+    -o '~/Videos/YouTube/%(upload_date)s - %(uploader)s - %(title)s.%(ext)s' \
     "$argv[1]"
 end
