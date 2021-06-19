e94f8475f7ca0f12ead0d463942a0104efebb61a
Tryton Van Meer <hello@trytonvanmeer.dev>
2021-06-19 00:29:28 -0400

    Use protontricks instead for searching appid.

--- roles/fish/files/functions/steam-get-appid.fish
+++
@@ -1,7 +0,0 @@
-function steam-get-appid -d "List possible appids with search term"
-    # Grep appmanifest files in all Steam libraries for games with $arg in name
-
-    grep -Ei "name.*$argv[1]" $steam_dirs \
-    /opt/Steam/steamapps/appmanifest_*.acf \
-    ~/.steam/steam/steamapps/appmanifest_*.acf
-end
