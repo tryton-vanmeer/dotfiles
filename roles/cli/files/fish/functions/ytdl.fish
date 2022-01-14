e5d3434680d0029a072b1bff91b1a9174258bfc1
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-01-13 19:51:00 -0500

    remove ytdl function.

--- roles/cli/files/fish/functions/ytdl.fish
+++
@@ -1,5 +0,0 @@
-function ytdl -w "youtube-dl"
-    youtube-dl -f '(bestvideo+bestaudio/best)[protocol^=http]' \
-    -o '~/Videos/YouTube/%(upload_date)s - %(uploader)s - %(title)s.%(ext)s' \
-    "$argv[1]"
-end
